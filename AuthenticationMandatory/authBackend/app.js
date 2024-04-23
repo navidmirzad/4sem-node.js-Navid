d6ccb9220b3fdc55be62e79c6fcedaa4516ceaff
navidmirzad <navi0221@stud.kea.dk>
2024-04-23 18:04:16 +0200

    mandatory II start

---
+++ AuthenticationMandatory/authBackend/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+
+app.use(express.json());
+
+const users = [];
+
+app.get("/login", (req, res) => {
+  // Authenticate User
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = { name: req.body.name, password: hashedPassword };
+    users.push(user);
+    res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  const user = users.find((user) => (user.name = req.body.name));
+  if (user == null) {
+    return res.status(400).send("Cannot find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("Success");
+    } else {
+      res.send("Not Allowed");
+    }
+  } catch {
+    res.status(500).send();
+  }
+});
+
+const PORT = 8080;
+app.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
