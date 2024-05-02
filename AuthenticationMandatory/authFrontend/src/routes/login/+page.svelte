0b2c90bd72843e313d99bbe50332d72aaa3403bf
navidmirzad <navi0221@stud.kea.dk>
2024-05-02 17:40:45 +0200

    POST /login form looks good, /profile/{username} nice

--- AuthenticationMandatory/authFrontend/src/routes/login/+page.svelte
+++ AuthenticationMandatory/authFrontend/src/routes/login/+page.svelte
@@ -1,1 +1,53 @@
-<h1 class="text-3xl">LOGIN</h1>
+<script>
+	let username = '';
+	let password = '';
+
+	async function login(event) {
+		event.preventDefault();
+
+		const user = {
+			username,
+			password
+		};
+
+		const response = await fetch('http://localhost:8080/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(user)
+		});
+
+		if (response.ok) {
+			alert('Login successful!');
+			window.location.href = `/profile/${username}`;
+		} else {
+			alert('Login failed, try again...');
+		}
+	}
+</script>
+
+<div class="container mx-auto max-w-md mt-20 justify-center border-2 border-black p-4">
+	<h1 class="text-3xl font-bold mb-4 flex justify-center">Login</h1>
+	<form on:submit={login}>
+		<div class="mb-4">
+			<label for="username" class="block text-gray-700">Username</label>
+			<input
+				type="text"
+				id="username"
+				bind:value={username}
+				class="border border-gray-300 px-4 py-2 rounded-md w-full"
+			/>
+		</div>
+		<div class="mb-4">
+			<label for="password" class="block text-gray-700">Password</label>
+			<input
+				type="password"
+				id="password"
+				bind:value={password}
+				class="border border-gray-300 px-4 py-2 rounded-md w-full"
+			/>
+		</div>
+		<button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md">Login</button>
+	</form>
+</div>
--- AuthenticationMandatory/authFrontend/src/routes/profile/[username]/+page.svelte
+++ AuthenticationMandatory/authFrontend/src/routes/profile/[username]/+page.svelte
@@ -1,8 +1,10 @@
 <script>
 	import { page } from '$app/stores';
-	const username = $page.params.username;
+	let username = $page.params.username;
+	username = username.charAt(0).toUpperCase() + username.slice(1);
 </script>
 
-<h1>Welcome to your profile {username}</h1>
-
-<button>Log out</button>
+<div class="mx-20 mt-20">
+	<h1 class="font-bold text-3xl mb-2">Welcome to your profile {username}</h1>
+	<p class="text-lg font-bold">This is your profile page. You can see your information here.</p>
+</div>
