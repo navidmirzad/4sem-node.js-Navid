1734df55366a5c7560809af027d0bb9f11ee3613
navidmirzad <navi0221@stud.kea.dk>
2024-05-02 12:12:19 +0200

    routes, basic styling and buttons down. Now to fetch APIs

--- AuthenticationMandatory/authFrontend/src/routes/+page.svelte
+++ AuthenticationMandatory/authFrontend/src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Authentication NodeJS & SvelteKit</h1>
-<h2>Mandatory II</h2>
+<!-- // -->
+<!--  -->
+<!--  -->
+<div class="mx-20 mt-20">
+	<h1 class="font-bold text-3xl mb-2">Auth in NodeJS & SvelteKit</h1>
+	<h2 class="font-bold text-3xl"><i>Mandatory II</i></h2>
+</div>
+
+<div>
+	<div class="mt-8">
+		<h1 class="font-bold flex justify-center text-3xl">Tech stack for this project</h1>
+		<div class="flex justify-center bg-[#ffff] mt-4 w-full">
+			<img
+				class="w-100 h-20 mt-2 mb-4 mr-4"
+				src="./src/public/images/nodejs.png"
+				alt="Node.JS Logo"
+			/>
+			<img
+				class="w-100 h-20 mt-4 mb-4 mr-4"
+				src="./src/public/images/express.png"
+				alt="Express Logo"
+			/>
+			<img class="w-100 h-20 mt-4 mb-4 mr-4" src="./src/public/images/mysql.png" alt="MySQL Logo" />
+			<img
+				class="w-100 h-20 mt-4 mb-4 mr-4"
+				src="./src/public/images/sveltekit.png"
+				alt="SvelteKit Logo"
+			/>
+		</div>
+	</div>
+</div>
